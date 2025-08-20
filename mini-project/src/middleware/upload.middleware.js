3eb8ab90892943f1878b32691aa24349a849fcfd
Ferifer <ferihandoyo04@gmail.com>
2025-08-20 22:40:39 +0700

    add limit size file

--- mini-project/src/middleware/upload.middleware.js
+++ mini-project/src/middleware/upload.middleware.js
@@ -33,6 +33,10 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({ storage, fileFilter });
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 2 * 1024 * 1024 },
+});
 
 module.exports = upload;
